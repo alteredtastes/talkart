0efee8532d22a8a2e48d8e08d0612b613d1e0e0b
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2016-06-24 01:29:51 -0700

    configured login db connectivity and navbar template

--- controllers/main-controller.js
+++ controllers/main-controller.js
@@ -5,9 +5,12 @@
     .module('talkart')
     .controller("MainController", MainController);
 
-  function MainController(Sketch) {
+  function MainController(Sketch, Login) {
     var vm = this;
     vm.message = 'you are on the main controller';
+    Login.getUser().then(function(data) {
+      vm.user = data;
+    })
   }
 
 })();
--- services/sketch.js
+++ services/sketch.js
@@ -8,7 +8,7 @@
   function Sketch(p5, Creator) {
     return function(p) {
       p.setup = function() {
-        p.createCanvas(640, 480);
+        p.createCanvas(800, 800);
         p.background(200,200,200);
       }
 
